044f894dabf22cfb9fa518c946ed715092f40d1b
chorres <chorres.rodriguez.jhonatan@gmail.com>
2024-07-08 13:45:03 -0500

    Variables y tipo de datos

---
+++ variable_tipo_datos.js
@@ -0,0 +1,33 @@
+//variable
+//string
+let x="Jhonatan";
+
+//numeros
+let num=25;
+let deci=25.5;
+
+//booleano
+let dato=true;
+let dato2=false;
+
+console.log(x);
+console.log(num);
+console.log(deci);
+console.log(dato);
+console.log(dato2);
+
+//array
+let dat=["Jhonatan","Jacinto","Rafael","Bety"];
+console.log(dat);
+
+//object
+let da={nombre:"jhonatan",apellido:"chorres",age:29,job:"developer"};
+console.log(da);
+
+//undefinido
+let r;
+console.log(r);
+
+//NULL ausencia de objeto o valor
+let user=null
+console.log(user);
